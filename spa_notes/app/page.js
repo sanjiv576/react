dbe43e41dca8823f2dd69365f4254203e46a6f45
Sanjiv Shrestha <sanjiv576>
2023-05-30 11:53:14 +0545

    add json file locally, connect API and frontend and made full-stack

--- spa_notes/app/page.js
+++ spa_notes/app/page.js
@@ -3,27 +3,12 @@ import Image from 'next/image'
 
 export default function Home() {
   {/* data structure */ }
-  const notes = [
-    {
-      id: 1,
-      desc: 'today is chill',
-      important: true
-    },
-    {
-      id: 2,
-      desc: 'today is fun',
-      important: false
-    },
-    {
-      id: 3,
-      desc: 'today is Thursday',
-      important: true
-    }
-  ]
+  
   return (
     <div>
 
-      <Notes notes={notes} />
+      {/* <Notes notes={notes} /> */}
+      < Notes/>
 
       
     </div>
--- spa_notes/components/Notes.js
+++ spa_notes/components/Notes.js
@@ -1,10 +1,14 @@
 'use client'
 
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const baseUrl = 'http://localhost:4000/notes';
 
 // parameters come from parent i.e Page
-export default function Notes(props) {
+export default function Notes() {
+
+
 
     // store notes object from parent to the notes
     // const { notes } = props;
@@ -13,12 +17,45 @@ export default function Notes(props) {
     const [desc, setDesc] = useState('');
 
     // change props as state for sending data from child to parent
-    const [notes, setNotes] = useState(props.notes);
+    const [notes, setNotes] = useState([]);
+
+    // to handle the state for showing either edit or save button
+    const [isEdit, setIsEdit] = useState(false);
+
+    // state to store which note is going to be edited
+    const [targetNote, setTargetNote] = useState();
+
+    // state for showing either important or  all button
+    const [showAll, setShowAll] = useState(true);
+
+    // another Hook to render the component --> called by React
+    useEffect(() => {
+
+        // fetch the data from the server 
+        axios.get(baseUrl)
+            .then(response => {
+                // console.log(response);
+                // console.log(response.data);
+
+                // send data
+                setNotes(response.data);
+
+            })
+
+        // NOte: , [] ===> is used to render only once, to see effect remove , [] and Go to browser ==> Inspect ==> Network 
+
+    }, []);
+
+    // storing either all or important notes  ==> same logic used for searching particular note
+    const filteredNotes = showAll ? notes
+        :
+        // only store imporant notes
+        notes.filter(note => note.important === true);
 
     // event is changed value from the user
     const handleChange = (event) => {
 
-        console.log(event.target.value);
+        // console.log(event.target.value);
         setDesc(event.target.value);
     };
 
@@ -30,35 +67,118 @@ export default function Notes(props) {
         // object of note to store desc
         // NOte: while working with API , we make object here
         const newNote = {
-            id: notes.length + 1,
+
+            // removed id, becuase server creates it
+            // id: notes.length + 1,
+
             // user input value desc is already in desc beacuse of descState
             desc: desc,
             // if random comes lesser than 0.5 then, it is true otherwise false
             important: Math.random() < 0.5
         }
 
-        console.log(newNote);
+        // console.log(newNote);
 
-        // push the newNote to the notes of parent
-        setNotes(notes.concat(newNote));
+        // now pushing data to the server
+        axios.post(baseUrl, newNote)
+            .then(response => {
+                console.log(response.data);
+                // push the newNote to the notes of parent to the local
+                setNotes(notes.concat(response.data));
+            })
+        setDesc('')
+
     };
 
-    const handleDelete = (noteId) => {
+    const handleEdit = (noteId) => {
         // alert(noteId);
-        const newDeletedNotes = notes.filter(note => note.id !== noteId);
-        // console.log(newDeletedNotes);
-        setNotes(newDeletedNotes);
+
+        // console.log(notes.find(note => note.id === noteId));
+        const targetNote = notes.find(note => note.id === noteId);
+        // set in the input form
+        setDesc(targetNote.desc);
+        // change the state
+        setIsEdit(true);
+        // set the state which note is going to be edited
+        setTargetNote(targetNote);
+
+    }
+
+    // Note: Save button is inside the form so we need event to prevent from the default button behaviour 
+    //i.e rendering the page which means resetting the state
+
+    const handleSave = (event) => {
+
+        // prevent from the resetting the state
+        event.preventDefault();
+
+
+
+        // update to the server
+        axios.put(`${baseUrl}/${targetNote.id}`,
+            // update those things to be updated
+            { ...targetNote, desc: desc })
+            .then(response => {
+                console.log(response);
+
+                // after success, also change in local
+                // edit/update the note
+                const updatedNotes = notes.map(note => note.id === targetNote.id ?
+                    { ...targetNote, desc: desc }
+                    : note);
+                setNotes(updatedNotes);
+
+            })
+
+
+        // reset the state of button and desc input
+        setIsEdit(false);
+        setDesc('');
+
+
+    }
+
+    // for toggling 
+    const handleImportant = () => {
+        setShowAll(!showAll)
+
+        // const importantNotes = notes.filter(note => note.important === true);
+        // // console.log(importantNotes);
+        // setNotes(importantNotes);
+    }
+
+
+    const handleDelete = (noteId) => {
+
+        if (window.confirm(`Are you sure to delete note with id ${noteId}`)) {
+            // alert(noteId);
+            const newDeletedNotes = notes.filter(note => note.id !== noteId);
+            // console.log(newDeletedNotes);
+
+            // sending data to the server
+            axios.delete(`${baseUrl}/${noteId}`)
+                .then(response => {
+                    console.log(response);
+
+                    // after only success
+                    setNotes(newDeletedNotes);
+
+                })
+        }
     }
     return (
         <>
             <h1>Welcome to Notes</h1>
 
+            <button onClick={handleImportant}> show {showAll ? 'important' : 'all'}</button>
+
             <ul>
                 {
-                    notes.map(note =>
+                    filteredNotes.map(note =>
                         <li key={note.id}>{note.desc}{'    '}
                             {/* send id as a parameter in handleDelete */}
                             <button onClick={(id) => handleDelete(note.id)}>delete</button >
+                            <button onClick={(id) => handleEdit(note.id)}>edit</button >
                         </li>
                     )
                 }
@@ -71,7 +191,15 @@ export default function Notes(props) {
                     value={desc}
                     onChange={handleChange}
                 />
-                <button onClick={handleAdd}>Add</button>
+
+                {
+                    isEdit ?
+                        <button onClick={handleSave}>Save</button>
+
+                        :
+                        <button onClick={handleAdd}>Add</button>
+
+                }
             </form>
         </>
     );
