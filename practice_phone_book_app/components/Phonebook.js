4293e61a431d1700a6faad65e91ece4b77410d37
Sanjiv Shrestha <sanjiv576>
2023-05-31 21:01:23 +0545

    connect with API and perform CRUD operations with it

--- practice_phone_book_app/components/Phonebook.js
+++ practice_phone_book_app/components/Phonebook.js
@@ -1,8 +1,10 @@
 'use client'
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const baseUrl = 'http://localhost:4000/contacts';
 
-// component for button
+// Step: 4 component for button
 const Button = (props) => {
     const { handleClick, text } = props;
     return (
@@ -10,7 +12,7 @@ const Button = (props) => {
     )
 };
 
-// component for input
+// Step 3:  component for input
 const Input = (props) => {
     const { handleChange, text, type, value } = props;
     return (
@@ -28,10 +30,11 @@ const Input = (props) => {
     )
 };
 
-// separate component for Retrieve and Update and Delete operations of the contacts details
+
+// Step 2:  separate component for Retrieve and Update and Delete operations of the contacts details
 const ContactDetails = (props) => {
 
-    const { contacts, name, phoneNumber, search, deleteClick, editClick } = props;
+    const { contacts, deleteClick, editClick } = props;
 
 
     return (
@@ -59,39 +62,69 @@ const ContactDetails = (props) => {
                             </tr>)
                     }
                 </tbody>
-
-
-
-
             </table>
         </>
     )
 };
 
 
-export default function Phonebook(props) {
+// Step 1: 
+export default function Phonebook() {
     // const [contacts, setContacts] = useState(props.contacts);
 
     const [phonebook, setPhonebook] = useState(
         {
-            contacts: props.contacts,
+            contacts: [],
             name: '',
 
             phoneNumber: 0,
-            search: ''
+            search: '',
+
+            isEdit: false,
+            targetContact: null
+
         }
     );
 
-    const handleSearchChange = (event) => {
-        setPhonebook(
-            {
-                ...phonebook,
-                // only updating serch
-                search: event.target.value
-            }
-        );
+    useEffect(() => {
 
-        console.log(phonebook.search);
+        axios.get(baseUrl)
+            .then(response => {
+                // console.log(response);
+                setPhonebook({
+                    ...phonebook,
+                    contacts: response.data
+                });
+            })
+    }, []);
+
+    const handleSearchChange = (event) => {
+
+        const searchingText = event.target.value;
+        // console.log(searchingText);
+        // console.log(typeof(searchingText));
+
+        const orginalConacts = phonebook.contacts;
+        const filteredContacts = phonebook.contacts.filter(contact => contact.name.includes(searchingText));
+
+
+        // update UI as well
+        setPhonebook({
+            ...phonebook,
+            search: searchingText,
+            contacts: filteredContacts
+        });
+
+
+
+
+        // if(searchingText === ''){
+        //     setPhonebook({
+        //         ...phonebook,
+        //         contacts: orginalConacts
+        //     })
+        // }
+
     };
 
 
@@ -100,7 +133,7 @@ export default function Phonebook(props) {
             ...phonebook,
             name: event.target.value
         });
-        console.log(phonebook.name);
+        // console.log(phonebook.name);
     }
 
     const handlePhoneNumChange = (event) => {
@@ -108,17 +141,112 @@ export default function Phonebook(props) {
             ...phonebook,
             phoneNumber: event.target.value
         });
-        console.log(phonebook.phoneNumber);
+        // console.log(phonebook.phoneNumber);
     }
 
     const handleEdit = (contactId) => {
-        alert(contactId);
+
+        const targetContact = phonebook.contacts.filter(contact => contact.id === contactId);
+
+        setPhonebook({
+            ...phonebook,
+            // show name and phone number data in the input
+            name: targetContact[0].name,
+            phoneNumber: targetContact[0].phone,
+
+            // change Add to Save in button
+            isEdit: true,
+
+            // set the state which contact is going to be edited
+            targetContact: targetContact
+        });
+
     };
+
+    const handleSave = (event) => {
+        // prevent from the resetting the state
+        event.preventDefault();
+
+        console.log(phonebook.targetContact[0].id);
+        const targetContactId = phonebook.targetContact[0].id;
+        console.log(targetContactId);
+
+        setPhonebook({
+            ...phonebook,
+            name: phonebook.name,
+            phoneNumber: phonebook.phoneNumber
+        });
+
+        // console.log(phonebook.name);
+        // console.log(phonebook.phoneNumber);
+
+        axios.put(`${baseUrl}/${targetContactId}`,
+            // NOte: key must be the same as in the db
+            { ...targetContactId, name: phonebook.name, phone: phonebook.phoneNumber }
+        )
+            .then(response => {
+                const updatedConttacts = phonebook.contacts.map(contact => contact.id === phonebook.targetContact[0].id ?
+                    { ...phonebook, name: phonebook.name, phone: phonebook.phoneNumber }
+                    : contact);
+
+                // console.log(updatedConttacts);
+
+                setPhonebook({
+                    ...phonebook,
+                    contacts: updatedConttacts,
+                    name: '',
+                    phoneNumber: 0,
+                    isEdit: false
+                });
+            })
+
+    }
 
     const handleDelete = (contactId) => {
-        alert(contactId);
+        // alert(contactId);
+
+        if (window.confirm('Are you sure want to delete ? ')) {
+            const newContacts = phonebook.contacts.filter(contact => contact.id !== contactId);
+
+            axios.delete(`${baseUrl}/${contactId}`)
+                .then(response => {
+                    // inwert new contacts after deleting and update UI as well
+                    setPhonebook({
+                        ...phonebook,
+                        contacts: newContacts
+                    });
+                })
+
+        }
 
     };
+
+    let handleAdd = (event) => {
+        event.preventDefault();
+        console.log(`Lenght of array: ${phonebook.contacts.length}`);
+        const newId = phonebook.contacts.length + 1;
+        const newContact = {
+            id: newId,
+            name: phonebook.name,
+            phone: phonebook.phoneNumber
+        };
+        // console.log(newContact);
+
+        axios.post(baseUrl, newContact)
+            .then(response => {
+
+                console.log(response);
+                // add new contact in the contacts
+
+                setPhonebook({
+                    ...phonebook,
+                    contacts: phonebook.contacts.concat(response.data),
+                    name: '',
+                    phoneNumber: 0
+                });
+            })
+    };
+
     return (
         <>
             <h1>Phonebook App</h1>
@@ -133,7 +261,7 @@ export default function Phonebook(props) {
             </form>
 
 
-            {/*  for adding contact */}
+            {/* Step 3:   for adding contact */}
 
             <h3>Add Contact</h3>
 
@@ -155,17 +283,23 @@ export default function Phonebook(props) {
                     type='number'
                     text='Phone'
                 />
+
+                {/* button */}
+
+                {
+                    phonebook.isEdit
+                        ?
+                        <Button handleClick={handleSave} text='Save' />
+                        :
+                        <Button handleClick={handleAdd} text='Add' />
+                }
             </form>
 
             <br />
 
-            {/*  for displaying contact details */}
+            {/* Step 2:  for displaying contact details */}
             < ContactDetails
-
                 contacts={phonebook.contacts}
-                name={phonebook.name}
-                phoneNumber={phonebook.phoneNUmber}
-                search={phonebook.search}
                 deleteClick={handleDelete}
                 editClick={handleEdit}
 
